4ea83ce7a05a48026251ebee9e1961d8b9721451
Rizwan Ishtiaq <rizwan.ishtiaq@hotmai.com>
2020-10-20 21:03:13 +0100

    deployment

---
+++ precache-manifest.836eadef99198f83d54771f99a6af896.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e233557863caa8c284b00aa39d39bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5200000fc50508ee14b4",
+    "url": "/static/css/2.870a4f90.chunk.css"
+  },
+  {
+    "revision": "bb55cd9bb3aa8c19c352",
+    "url": "/static/css/main.f692d667.chunk.css"
+  },
+  {
+    "revision": "5200000fc50508ee14b4",
+    "url": "/static/js/2.5a8dc6c9.chunk.js"
+  },
+  {
+    "revision": "e6e7245227d6e1b15693f6c87ec245fa",
+    "url": "/static/js/2.5a8dc6c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb55cd9bb3aa8c19c352",
+    "url": "/static/js/main.60c03c73.chunk.js"
+  },
+  {
+    "revision": "f31f99603d73a3885e80",
+    "url": "/static/js/runtime-main.f14c19f3.js"
+  },
+  {
+    "revision": "94a3225c9cfaf8990486dc58a28e33ae",
+    "url": "/static/media/checkbox-1.94a3225c.svg"
+  },
+  {
+    "revision": "37b21b36215d35b3d07c8efe200f89e0",
+    "url": "/static/media/checkbox-2.37b21b36.svg"
+  },
+  {
+    "revision": "40b4626e52de3901a295c7648e51d939",
+    "url": "/static/media/f22bg.40b4626e.png"
+  },
+  {
+    "revision": "50a4ab76e700a83e649be213f820fbbd",
+    "url": "/static/media/icofont.50a4ab76.woff2"
+  },
+  {
+    "revision": "f6ab04aed30a8643bf94fe00f7ff0b59",
+    "url": "/static/media/icofont.f6ab04ae.woff"
+  },
+  {
+    "revision": "814ee3f03e6a8f4d82ba8098097ab324",
+    "url": "/static/media/radio-1.814ee3f0.svg"
+  },
+  {
+    "revision": "43810451826ac2f7870b091eb9a29390",
+    "url": "/static/media/radio-2.43810451.svg"
+  },
+  {
+    "revision": "6be21e8a1b7d63048728851c6003e189",
+    "url": "/static/media/sidebar-1.6be21e8a.jpg"
+  },
+  {
+    "revision": "7d85cc33dd6679b1a283d76242e2867a",
+    "url": "/static/media/zebo-logo.7d85cc33.png"
+  }
+]);
